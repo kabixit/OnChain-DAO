bd2112b844a0f271867ba4be1473352690190e1c
Kabilan <kumarakabilan2004@gmail.com>
2023-10-04 02:12:45 +0530

    cmt

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -362,7 +362,7 @@ export default function Home() {
           )}
         </div>
         <div>
-          <img className={styles.image} src="https://i.imgur.com/buNhbF7.png" />
+          <p>Made with ❤️ by Kabix</p>
         </div>
       </div>
     </div>
